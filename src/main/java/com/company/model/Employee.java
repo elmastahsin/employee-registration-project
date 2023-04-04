4e3daa9530f313a30a44f53420a26edba13dbbc3
tahsinelmas <elmas.tahsin@metu.edu.tr>
2023-04-05 01:59:47 +0300

    EmployeeService created

--- src/main/java/com/company/model/Employee.java
+++ src/main/java/com/company/model/Employee.java
@@ -14,6 +14,7 @@ import java.time.LocalDate;
 public class Employee {
     private String firstName;
     private String lastName;
+    // Thymeleaf accepts  yyyy-MM-dd but LocalDate accepts mm-dd-yyyy or yyyy-dd-mm
     private LocalDate birthday;
     private String email;
     private String password;
